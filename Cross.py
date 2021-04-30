2019b952817445e8fce92e201f1252b4ebf687b7
Vishal7017 <wavhalev@gmail.com>
2021-05-01 00:28:38 +0530

    Test

--- Cross.py
+++ Cross.py
@@ -2,6 +2,36 @@ def cross(A, B):
     "Cross product of elements in A and B"
     return [a+b for a in A for b in B]
 
-row = '12345678'
-cols = 'ABCEDFGHI'
-print(cross(row,cols))
+digits = '12345678'
+rows = 'ABCEDFGHI'
+cols = digits
+# print(cross(row,cols))
+
+squares = cross(rows, cols)
+
+unitlist = [[cross(rows, c) for c in cols]+
+            [cross(r, cols) for r in rows]+
+            [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123','456','789')]]
+
+
+units = dict((s, [u for u in unitlist if s in u]) for s in squares)
+
+peers = dict((s, set(sum(units[s], []))-set([s])) for s in squares)
+
+
+def test():
+    "A set of unit tests."
+    assert len(squares) == 81
+    assert len(unitlist) == 27
+    assert all(len(units[s]) == 3 for s in squares)
+    assert all(len(peers[s]) == 20 for s in squares)
+    assert units['C2'] == [['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+                           ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
+                           ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
+    assert peers['C2'] == set(['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
+                               'C1', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
+                               'A1', 'A3', 'B1', 'B3'])
+    print ('All tests pass.')
+
+
+test()
